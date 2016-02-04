d434a4b314be4d758e36c60e238614a9f3af15b6
Denis Kharitonov <Denis@MacBook-Pro-Denis-Kharitonov.local>
2016-02-04 23:21:29 +0300

    init

---
+++ clean-swift-live/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  clean-swift-live
+//
+//  Created by Denis Kharitonov on 04.02.16.
+//  Copyright © 2016 dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ clean-swift-live/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  clean-swift-live
+//
+//  Created by Denis Kharitonov on 04.02.16.
+//  Copyright © 2016 dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
